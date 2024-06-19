0ec24174818727b82b189789a9c4ae9e66be2483
Nagaprakash V <naga@scrapehero.com>
2024-06-19 20:04:17 +0530

    direct versioning

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup, find_namespace_packages
 from glob import glob
-from os import path
-
-# Get the version from Version.txt
-vers = path.abspath(path.dirname(__file__))
-with open(path.join(vers, 'VERSION.txt'), encoding='utf-8') as f:
-    __version__ = f.read()
 
 setup(name='edexplore',
-      version=__version__,
+      version=1.0,
       description='A simple widget for interactive data exploration for those who use Pandas in Jupyter Notebook.',
       install_requires=['requests', 'numpy', 'pandas', 'ipywidgets'],
       url='https://github.com/nagaprakashv/explore/',
