ba35ae912d437086399457823d58da4954945603
Nagaprakash V <nagaprakash@outlook.com>
2024-06-23 20:16:51 +0530

    version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 from glob import glob
 
 setup(name='edexplore',
-      version="1.0.0",
+      version="1.0.1",
       description='A simple widget for interactive EDA / QA for those who use Pandas in Jupyter Notebook.',
       install_requires=['numpy>=1.25.2', 'pandas>=2.0.3', 'ipywidgets>=8.1.0', 'notebook>=7.0.2'],
       url='https://github.com/nagaprakashv/edexplore/',
