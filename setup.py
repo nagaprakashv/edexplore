9144c376efa18129ce87f79f066f3980c221ba99
Nagaprakash V <nagaprakash@outlook.com>
2024-06-23 19:44:40 +0530

    desc change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setup(name='edexplore',
       version="1.0.0",
-      description='A simple widget for interactive data exploration for those who use Pandas in Jupyter Notebook.',
+      description='A simple widget for interactive EDA / QA for those who use Pandas in Jupyter Notebook.',
       install_requires=['numpy>=1.25.2', 'pandas>=2.0.3', 'ipywidgets>=8.1.0', 'notebook>=7.0.2'],
       url='https://github.com/nagaprakashv/edexplore/',
       author='nagaprakash venkatesan',
